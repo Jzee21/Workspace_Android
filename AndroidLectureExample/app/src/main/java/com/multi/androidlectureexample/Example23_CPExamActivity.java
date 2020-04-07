520cfd504fe8cd59368c4d2779d2fe533e8a814d
Jzee21 <Jzee21b@gmail.com>
2020-04-07 15:56:41 +0900

    Content Provider (with SQLite)

--- AndroidLectureExample/app/src/main/java/com/multi/androidlectureexample/Example23_CPExamActivity.java
+++ AndroidLectureExample/app/src/main/java/com/multi/androidlectureexample/Example23_CPExamActivity.java
@@ -1,14 +1,94 @@
 package com.multi.androidlectureexample;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+/*
+*   Content Provider (내용제공자)
+*   App 에서 관리하는 데이터(SQLite DB)를 다른 App이 접근할 수 있도록 하는 기능
+*   CP 는 Android 구성요소 중 하나로 안드로이드 시스템에 의해 관리된다.
+*
+*   AndroidManifest.xml 파일에 등록하여 사용
+*
+*   CP가 필요한 이유는 보안규정 때문
+*   Android App 은 오직 자신의 App 안에서 생성한 데이터만 사용할 수 있다.
+*   다른 App 이 가진 데이터의 접근권한은 없다
+*
+*   CP를 이용하면 다른 App 의 데이터에 접근할 수 있다.
+*   (일반적으로 Database 에 접근하는 방식을 이용)
+*   그 이유는 CP 가 CRUD 를 기반으로 하고있기 때문이다.
+*
+*   1. 데이터베이스(SQLite)를 이용하기 때문에 SQLiteOpenHelper class 를 이용한다
+*
+*
+* */
 public class Example23_CPExamActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_example23_cp_exam);
+
+        // _23_empNameEt
+        // _23_empAgeEt
+        // _23_empMobileEt
+
+        // _23_empInsertBtn
+        Button _23_empInsertBtn = findViewById(R.id._23_empInsertBtn);
+        _23_empInsertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 원래는 Activity에서 데이터를 가져오겠지만... ㅎ
+
+                // CP 탐색
+                String uriString = "content://com.multi.person.provider/person";
+                Uri uri = new Uri.Builder().build().parse(uriString);
+
+                // HashMap 형태로 DB 에 입력할 데이터를 저장
+                ContentValues values = new ContentValues();
+                values.put("name", "홍길동");
+                values.put("age", 20);
+                values.put("mobile", "010-1111-5555");
+
+                getContentResolver().insert(uri, values);
+                Log.i("DBTest", "Insert Data");
+            }
+        });
+
+        //
+        // _23_empSelectBtn
+        // _23_resultTv
+    }
+}
+
+class PersonDatabaseHelper extends SQLiteOpenHelper {
+
+    public PersonDatabaseHelper(@Nullable Context context) {
+        super(context, "person.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // DB 생성 시점에 Table도 같이 생성
+        String sql = "CREATE TABLE IF NOT EXISTS "
+                + "person(_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "name TEXT, age INTEGER, mobile TEXT)";
+        db.execSQL(sql);
+        Log.i("DBTest", "onCreate()");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
     }
 }
---
+++ AndroidLectureExample/app/src/main/java/com/multi/androidlectureexample/Example23sub_PersonContentProvider.java
@@ -0,0 +1,80 @@
+package com.multi.androidlectureexample;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.util.Log;
+
+public class Example23sub_PersonContentProvider extends ContentProvider {
+
+    /*  URI 형식 ( Content Provider를 찾기위한 특별한 문자열 )
+        content://{Authority}/{BASE_PATH}/{#no}
+        - BASE_PATH : Table 명
+        - #no       : recode 번호
+
+        ex) content://com.multi.person.provider/person/1
+     */
+
+    private SQLiteDatabase database;
+
+    public Example23sub_PersonContentProvider() {
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // Implement this to handle requests to delete one or more rows.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        // TODO: Implement this to handle requests for the MIME type of the data
+        // at the given URI.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        // TODO: Implement this to handle requests to insert a new row.
+        // throw new UnsupportedOperationException("Not yet implemented");
+
+        Log.i("DBTest", "CP_insert()");
+
+        // database 에 insert 하는 방법 중 하나 (SQL 문 없이)
+        // ContentValues values  : key, value 형태로 데이터 묘사
+        database.insert("person",null, values);
+        return uri;
+    }
+
+    @Override
+    public boolean onCreate() {
+        // TODO: Implement this to initialize your content provider on startup.
+        // return false;
+
+        // 앱 실행 시 Android System에 의해서 Content Provider가 자동적으로 생성된다.\
+        // 여기에 Database 생성 및 Table 생성 코드가 오면 된다
+        // 또한, DB Reference를 획득하는 코드도 오면 된다.
+        Log.i("DBTest", "CP_onCreate()");
+
+        PersonDatabaseHelper helper = new PersonDatabaseHelper(getContext());
+        database = helper.getWritableDatabase();
+
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        // TODO: Implement this to handle query requests from clients.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        // TODO: Implement this to handle requests to update one or more rows.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
